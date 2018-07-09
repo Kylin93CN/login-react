9bf9d38c9bd69c67df12a322017f2be6a3439d81
Kylin93CN <375020612@qq.com>
2018-07-09 09:26:48 +0800

    服务端接口问题修改

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import users from './routes/user';
 
 let app = express();
-app.use('api/users',users);
+app.use('/api/users',users);
 
 app.get('/',(req,res) =>{
     res.send('hello port 6060!'); 
