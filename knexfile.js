8e869560ec8273984fa71dc5849c7c44da987b42
Kylin93CN <375020612@qq.com>
2018-07-11 13:58:53 +0800

    knex--IP设置

--- knexfile.js
+++ knexfile.js
@@ -5,6 +5,7 @@ module.exports = {
   development: {
     client: 'postgresql',
     connection: {
+      host : '127.0.0.1',
       database: 'reduxlogin',
       user:     'postgres',
       password: 'postgres'
