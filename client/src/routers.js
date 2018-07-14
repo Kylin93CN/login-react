9e63b1e42b83c02defa628beb2e1d0ab2e04cff6
Kylin93CN <375020612@qq.com>
2018-07-14 21:23:41 +0800

    添加高阶组件对无权限路由进行控制跳转

--- client/src/routers.js
+++ client/src/routers.js
@@ -6,11 +6,13 @@ import SignupPage from './components/signup/SignupPage';
 import LoginPage from './components/login/LoginPage';
 import AddSomethingPage from './components/others/AddSomethingPage';
 
+import requireAuth from './utils/requireAuth';
+
 export default(
   <div className="container">
     <Route exact path="/" component={ App } />
     <Route path="/signup" component={ SignupPage } />
     <Route path="/login" component={ LoginPage } />
-    <Route path="/addSomething" component={ AddSomethingPage } />
+    <Route path="/addSomething" component={ requireAuth(AddSomethingPage) } />
   </div>
 )
